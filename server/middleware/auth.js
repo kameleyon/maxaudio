3bd13daf9579ebda1108101211a6d95cb394b471
lightspiritux <184447011+lightspiritux@users.noreply.github.com>
2024-11-15 15:31:31 +0000

    login issues fixed start:all

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const User = require('../models/user.model');
+const { User } = require('../models/user.model');
 
 const requireAuth = async (req, res, next) => {
   try {
